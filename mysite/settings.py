30d06a288af70d5ab53f37cbff5950589de8ea19
Nicolas <nicolasgrunwald@yahoo.dk>
2021-10-01 11:18:18 +0200

    redid the template for practice and added .css file

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'da-DK'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'Europe/Copenhagen'
 
